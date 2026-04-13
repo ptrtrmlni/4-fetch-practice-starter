7ea1ea4df35b308ada500c969fb1ae6445adb26e
ptrtrmlni <putritrimailani.ptma@gmail.com>
2026-04-13 21:55:29 +0700

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,42 @@
+/**
+ * Data restoran diambil dari RESTful API:
+ *  https://restaurant-api.dicoding.dev/
+ *
+ * Silakan baca dokumentasi untuk mengetahui-
+ * cara penggunaannya.
+ */
+
+async function getRestaurants() {
+  // TODO 1
+  const response = await fetch('https://restaurant-api.dicoding.dev/list');
+  const responseJson = await response.json();
+  return responseJson.restaurants;
+}
+
+async function getRestaurant(id) {
+  // TODO 2
+  const response = await fetch(`https://restaurant-api.dicoding.dev/detail/${id}`);
+  const responseJson = await response.json();
+  return responseJson.restaurant;
+}
+
+async function postRestaurantReview(id, name, review) {
+  // TODO 3
+  const response = await fetch('https://restaurant-api.dicoding.dev/review', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      id,
+      name,
+      review
+    })
+  });
+
+  const responseJson = await response.json();
+
+  return responseJson.customerReviews;
+}
+
+export { getRestaurant, getRestaurants, postRestaurantReview };
